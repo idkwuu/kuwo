8dd26b01bf3fa2a2027246520e36c83582475e90
co <97562462+idkwuu@users.noreply.github.com>
2026-02-17 20:14:28 +0100

    chore: improve console title and status messages

--- kuwo/Program.cs
+++ kuwo/Program.cs
@@ -6,11 +6,15 @@ class Program
 {
     static async Task Main(string[] args)
     {
+        // Console Title
+        Console.Title = "Kuwo | Made by ＳΛＲＴΞ";
+
+        // Show ASCII
         Ascii.ASCII();
 
         string id;
         string filename;
-        string format = "mp3";
+        string format = "mp3"; // Feature: Changeable music format
 
         if (args.Length < 2)
         {
@@ -38,8 +42,9 @@ class Program
         bool success = await task.DownloadAsync(url, fullPath);
 
         if (success)
-            Console.WriteLine("[SYS]: Download completed!"); // open explorer .....
+            Console.WriteLine("[SYS]: Download successful! Launching File Explorer..."); // open explorer .....
         else
+            Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.WriteLine("[SYS]: Download failed.");
 
         Console.ReadKey(true);
